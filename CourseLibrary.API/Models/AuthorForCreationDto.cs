f6881e04013732bedb3c93426ad7c14bc5b97745
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-10-20 13:19:41 -0400

    ICollection<> and List<>

--- CourseLibrary.API/Models/AuthorForCreationDto.cs
+++ CourseLibrary.API/Models/AuthorForCreationDto.cs
@@ -7,12 +7,15 @@ namespace CourseLibrary.API.Models
 {
     public class AuthorForCreationDto
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTimeOffset DateOfBirth { get; set; }
-        public string MainCategory { get; set; }
-        public ICollection<CourseForCreationDto> Courses { get; set; }
-          = new List<CourseForCreationDto>();
+        public string FirstName                             { get; set; }
+        public string LastName                              { get; set; }
+        public DateTimeOffset DateOfBirth                   { get; set; }
+        public string MainCategory                          { get; set; }
+        
+        
+        /* Notice how ICollection<> and List<> are used **********************************************     */
+        public ICollection<CourseForCreationDto> Courses    { get; set; }  
+                    = new List<CourseForCreationDto>();
 
     }
 }
